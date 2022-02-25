f5765daf35a4648f54daeba193ba07bfb89497c1
Paul <pechaffin@pm.me>
2022-02-25 10:03:37 -0500

    MD 25-02-2022

---
+++ ActorList.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+class Actor
+{
+  String name;
+  String role;
+  String birthdate;
+}
+
+public class ActorList
+{
+  public static void main( String[] args ) throws Exception
+  {
+    String url = "https://learnjavathehardway.org/txt/s01e01-cast.txt";
+    //Scanner inFile = new Scanner(new java.io.File("s01e01-cast.txt"));
+    Scanner inFile = new Scanner((new java.net.URL(url)).openStream());
+
+    while ( inFile.hasNext() )
+    {
+      Actor a = getActor(inFile);
+      System.out.print( a.name + " was born on " + a.birthdate );
+      System.out.println( " and played " + a.role );
+    }
+    inFile.close();
+  }
+
+  public static Actor getActor( Scanner input )
+  {
+    Actor a = new Actor();
+    a.name = input.nextLine();
+    a.role = input.nextLine();
+    a.birthdate = input.nextLine();
+
+    return a;
+  }
+}
